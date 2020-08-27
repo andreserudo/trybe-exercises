0420a7889108bb4213fa982b2b0d2cba08c4ec0d
Andre Serudo <andreserudo@gmail.com>
2020-08-27 16:53:52 -0400

    if statement

---
+++ FrontEnd/bloco_4/dia_1/trybe02.js
@@ -0,0 +1,16 @@
+/*
+Faça um programa que retorne o maior de dois números. Defina no começo do 
+programa duas variáveis com os valores que serão comparados.
+*/
+
+const a=2;
+const b=4;
+
+console.log('Sejam a = ' + a + ' e b = ' + b);
+
+a > b ? console.log('a é maior') : console.log('b é maior');
+
+if(a>b)
+  console.log('a é maior');
+else
+  console.log('b é maior');
