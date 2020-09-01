876d7e248080caa1e5920e3f01269170621f197a
Andre Serudo <andreserudo@gmail.com>
2020-09-01 17:27:57 -0400

    learning about objects - ex3

---
+++ FrontEnd/bloco_4/dia_4/part_1/trybe03.js
@@ -0,0 +1,24 @@
+/*
+Usando o objeto abaixo, faça os exercícios a seguir:
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+Faça um for/in que mostre todas as chaves do objeto.
+  Valor esperado no console:
+  personagem
+  origem
+  nota
+  recorrente
+*/
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: 'Sim',
+};
+
+for (let key in info) {
+  console.log(key);
+}
