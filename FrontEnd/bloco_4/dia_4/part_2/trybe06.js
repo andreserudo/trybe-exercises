a3497d1b9f58552d9116d68098cd33929106ee44
Andre Serudo <andreserudo@gmail.com>
2020-09-01 18:20:52 -0400

    learning about functions - ex6

---
+++ FrontEnd/bloco_4/dia_4/part_2/trybe06.js
@@ -0,0 +1,20 @@
+/*
+Crie uma função que receba um número inteiro N e retorne 
+o somatório de todos os números de 1 até N.
+Valor de teste: N = 5.
+Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+*/
+
+function somatorio(valor){
+  let soma = 0;
+
+  for (let index = 0; index <= valor; index += 1) {
+    soma += index;    
+  }
+
+  return soma;
+}
+
+let numero = 5;
+
+console.log(somatorio(numero));
