dde38c2eca02aa64719048cc05955b2377f03d3d
Andre Serudo <andreserudo@gmail.com>
2020-09-01 17:30:22 -0400

    learning about objects - ex4

--- FrontEnd/bloco_4/dia_4/part_1/trybe04.js
+++ FrontEnd/bloco_4/dia_4/part_1/trybe04.js
@@ -6,20 +6,20 @@ let info = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   recorrente: 'Sim'
 };
-Faça um for/in que mostre todas as chaves do objeto.
-  Valor esperado no console:
-  personagem
-  origem
-  nota
-  recorrente
+Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+Valor esperado no console:
+  Margarida
+  Pato Donald
+  Namorada do personagem principal nos quadrinhos do Pato Donald
+  Sim
 */
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  recorrente: 'Sim',
+  recorrente: 'Sim'
 };
 
 for (let key in info) {
-  console.log(key);
+  console.log(info[key]);
 }
