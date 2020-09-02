74ae8e907eb94cd462fafeae06c4e4e8115345d2
Andre Serudo <andreserudo@gmail.com>
2020-09-01 22:22:22 -0400

    learning about functions - ex7

--- FrontEnd/bloco_4/dia_4/part_2/trybe05.js
+++ FrontEnd/bloco_4/dia_4/part_2/trybe05.js
@@ -2,7 +2,11 @@
 Crie uma função que receba um array de inteiros e 
 retorne o inteiro que mais se repete.
 
-Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+  Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 
 Valor esperado no retorno da função: 2.
 */
+
+function maiorRepeticao(vetor){
+  
+}
