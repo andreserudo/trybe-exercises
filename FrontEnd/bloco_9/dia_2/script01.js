e902ddc29dbf0d6588c22df8e98826e21c061747
Andre Serudo <andreserudo@gmail.com>
2020-10-22 18:00:59 -0400

    exercicio 01

--- FrontEnd/bloco_9/dia_2/script01.js
+++ FrontEnd/bloco_9/dia_2/script01.js
@@ -1,7 +1,18 @@
 const API_URL = 'https://icanhazdadjoke.com/';
+const jokeContainer = document.querySelector('#jokeContainer');
 
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+  
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => {      
+      jokeContainer.innerText = data.joke;
+    });
 };
 
-window.onload = () => fetchJoke();
+fetchJoke();
