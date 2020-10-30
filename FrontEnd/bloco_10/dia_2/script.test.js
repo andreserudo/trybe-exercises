44d4c5f4856aef1e15f471830815602c78bf54d9
Andre Serudo <andreserudo@gmail.com>
2020-10-30 11:48:19 -0400

    exercicio 02 - resolucao

--- FrontEnd/bloco_10/dia_2/script.test.js
+++ FrontEnd/bloco_10/dia_2/script.test.js
@@ -11,16 +11,31 @@ const users = {
   5: { name: 'Paul' }
   };
   
-  const findUserById = (id) => {
-    return new Promise((resolve, reject) => {
-        if (users[id]) {
-          return resolve(users[id]);
-        }
-  
-        return reject({ error: 'User with ' + id + ' not found.' });
-    });
-  }
-  
-  const getUserName = (userID) => {
-    return findUserById(userID).then(user => user.name);
-  }
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+
+describe(`about getUserName function: `, () => {
+  it('should be able to return an username from an ID', () => {    
+    return getUserName(4).then(user => {
+      expect(user).toBe('Mark');
+    })
+  });
+
+  it('should be able to return an error message when user is not found', () => {    
+    return getUserName(1).catch(error => {      
+      expect(error).toEqual({ error: 'User with 1 not found.'});
+    })
+  });
+
+});
