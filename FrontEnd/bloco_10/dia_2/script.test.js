599a3d6a4e9fff1e00f579b03eb58410d8f28e32
Andre Serudo <andreserudo@gmail.com>
2020-10-30 11:49:48 -0400

    exercicio 03 - enunciado

--- FrontEnd/bloco_10/dia_2/script.test.js
+++ FrontEnd/bloco_10/dia_2/script.test.js
@@ -4,6 +4,10 @@
   e também um teste para o caso em que o usuário não é encontrado.
   Dica: Veja os dados falsos utilizados no banco de dados, disponíveis na 
   variável users, para saber quais IDs existem.
+
+  3. Reescreva o teste do exercício anterior, desta vez 
+  utilizando a sintaxe de async/await.
+  Dica: Utilize o try/catch para o caso de erro  
 */
 
 const users = {
