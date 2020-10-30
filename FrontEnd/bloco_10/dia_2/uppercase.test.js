6f28b4164f04c62bcebfd0e5f4a079a6555546dd
Andre Serudo <andreserudo@gmail.com>
2020-10-30 11:05:31 -0400

    dia 10.2 - jest com promisses

---
+++ FrontEnd/bloco_10/dia_2/uppercase.test.js
@@ -0,0 +1,11 @@
+/*
+  Escreva um teste que verifique a chamada do callback de uma 
+  função uppercase, que transforma as letras de uma palavra em 
+  letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos 
+  em testes assíncronos.
+*/
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+}
+
+console.log(uppercase('ttest', (element) => element));
